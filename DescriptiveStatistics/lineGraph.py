b225fa25a5f515e7daa47f18e9816228d0fe9465
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2023-06-20 16:10:43 +0200

    Update lineGraph.py

--- DescriptiveStatistics/lineGraph.py
+++ DescriptiveStatistics/lineGraph.py
@@ -1,1 +1,11 @@
+import matplotlib.pyplot as plt
 
+stipendio_iniziale = [27, 28, 29, 30, 31, 32, 34, 36, 37, 40]
+frequenza = [4, 1, 3, 5, 8, 10, 5, 2, 3, 1]
+
+plt.bar(stipendio_iniziale, frequenza)
+plt.xlabel('Stipendio Iniziale')
+plt.ylabel('Frequenza')
+plt.title('Grafico a Bastoncini degli Stipendi Iniziali')
+
+plt.show()
