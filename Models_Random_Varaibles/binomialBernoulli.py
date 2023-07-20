d351eb02fe075e3f91b0e3079032cf3fcdee8263
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2023-07-20 09:03:20 +0200

    Create binomialBernoulli.py

---
+++ Models_Random_Varaibles/binomialBernoulli.py
@@ -0,0 +1,9 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from scipy.stats import binom, bernoulli
+
+# Parametri delle variabili aleatorie
+n = 10  # Numero di tentativi
+p1 = 0.3  # Probabilità di successo per la variabile aleatoria 1
+p2 = 0.5  # Probabilità di successo per la variabile aleatoria 2
+p3 = 0.7  # Probabilità di successo per la variabile aleatoria 3
