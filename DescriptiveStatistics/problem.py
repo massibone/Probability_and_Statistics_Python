ffe92a556b97e175feac2e2417254c5d3e15abac
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2023-07-24 08:08:35 +0200

    Create problem.py

---
+++ DescriptiveStatistics/problem.py
@@ -0,0 +1,10 @@
+'''
+costruire: 
+a)tabella frequenze, 
+b) grafico a linee delle frequenze, 
+C) grafico frequenze cumulative relative 
+d)media campionaria 
+e)mediana campionaria 
+f)moda campionaria 
+g) deviazione standard campionaria?
+'''
