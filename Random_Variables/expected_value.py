530aa43de0c58ba165a80daaa1eb522d60efdee8
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2023-08-06 00:11:15 +0200

    Create expected_value.py

---
+++ Random_Variables/expected_value.py
@@ -0,0 +1,3 @@
+'''
+una compagnia di assicurazione emette una polizza che garantisce che verrà pagata una cifra A,in caso si verifichi un evento E entro un anno.Se la compagnia stimas che questo evento accada entro l'anno con probabilità P,quanto deve far pagare la polizza al cliente per avere un ricavo il cui valore atteso sia il 10% di A?
+'''
