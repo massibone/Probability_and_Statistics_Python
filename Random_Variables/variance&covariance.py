26abaf683627acb3520418df959c8851fa9802a4
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2023-08-05 18:33:07 +0200

    Update variance&covariance.py

--- Random_Variables/variance&covariance.py
+++ Random_Variables/variance&covariance.py
@@ -14,3 +14,20 @@ covarianza = np.cov(x, y)[0, 1]
 # Creazione del grafico
 fig, ax = plt.subplots()
 ax.scatter(x, y, color='b', label='Dati')
+# Aggiunta di etichette
+ax.set_xlabel('X')
+ax.set_ylabel('Y')
+
+# Aggiunta della legenda
+ax.legend()
+
+# Aggiunta del titolo
+ax.set_title('Varianza e Covarianza')
+
+# Aggiunta delle informazioni sulla varianza e la covarianza
+ax.text(1, 7, f'Varianza X: {varianza_x:.2f}', fontsize=8)
+ax.text(1, 6.5, f'Varianza Y: {varianza_y:.2f}', fontsize=8)
+ax.text(1, 6, f'Covarianza: {covarianza:.2f}', fontsize=8)
+
+# Mostra il grafico
+plt.show()
