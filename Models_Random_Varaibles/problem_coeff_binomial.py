e9c5ab55627d4d550b402c93162eb29365728761
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2023-08-09 13:26:56 +0200

    Update problem_coeff_binomial.py

--- Models_Random_Varaibles/problem_coeff_binomial.py
+++ Models_Random_Varaibles/problem_coeff_binomial.py
@@ -1,1 +1,5 @@
+'''
+calcolare la probabilità che vengano chiamati esattamente 2 tecnici su un totale di 4 tecnici disponibili, dobbiamo considerare il numero totale di modi in cui possiamo selezionare 2 tecnici su 4. Questo può essere calcolato utilizzando il coefficiente binomiale.
 
+Il coefficiente binomiale "n choose k" (indicato come nCk o C(n,k)) rappresenta il numero di modi in cui possiamo selezionare k elementi da un insieme di n elementi senza tener conto dell'ordine.
+'''
