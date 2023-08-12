7a473812398f4e57bd1a6e44f2fa27240e530db8
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2023-08-12 07:35:49 +0200

    Update Problem4.py

--- Models_Random_Varaibles/Problem4.py
+++ Models_Random_Varaibles/Problem4.py
@@ -1,1 +1,9 @@
+'''
+calcolare il numero minimo di persone necessarie affinché sia almeno il 50% la probabilità che qualcuno sia nato il 29 febbraio, possiamo utilizzare l'approccio del complemento. Calcoleremo il numero minimo di persone necessarie affinché la probabilità che nessuno sia nato il 29 febbraio sia inferiore al 50%, quindi useremo il complemento di questa probabilità.
+'''
 
+
+def calcola_probabilita(n):
+    probabilita = 1.0
+    giorni_natali = 365
+  
