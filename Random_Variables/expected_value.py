4c727bc4e61ea7112b58054844f6e558d00d1d10
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2023-08-15 10:04:59 +0200

    Update expected_value.py

--- Random_Variables/expected_value.py
+++ Random_Variables/expected_value.py
@@ -1,3 +1,9 @@
 '''
 una compagnia di assicurazione emette una polizza che garantisce che verrà pagata una cifra A,in caso si verifichi un evento E entro un anno.Se la compagnia stimas che questo evento accada entro l'anno con probabilità P,quanto deve far pagare la polizza al cliente per avere un ricavo il cui valore atteso sia il 10% di A?
 '''
+
+'''
+Il valore atteso (expected value) di una variabile casuale rappresenta la somma dei prodotti tra i possibili valori della variabile e le probabilità associate a tali valori.
+
+In questo caso, il ricavo della compagnia di assicurazione è rappresentato dalla cifra che la compagnia incassa dalla polizza, che chiameremo C. Quindi dobbiamo trovare la cifra C tale che il valore atteso del ricavo sia il 10% di A.
+
