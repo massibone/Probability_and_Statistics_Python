1d38001f39bb7d104e69eb5ac4b3660dee038b10
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2023-08-21 07:20:34 +0300

    Update problem1.py

--- Random_Variables/problem1.py
+++ Random_Variables/problem1.py
@@ -1,1 +1,6 @@
-
+Due giocatori disputano una serie di partite che tewrmina solo quando 
+uno dei due arriva a vincere I. Supponiamo che ogni partita venga
+vinta(indendentemente dalle altre) dal primo giocatore con probabilità P 
+e dal secondo con probabilità 1-P. 
+Trova il numero medio di partite disputyate se i=2. 
+Dimostra che questo valore è massimo se si pone P=1/2
