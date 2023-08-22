424c517ecb6fc8ff01a04f77b9cfd8bb959c8906
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2023-08-22 07:23:05 +0300

    Update problem1.py

--- Random_Variables/problem1.py
+++ Random_Variables/problem1.py
@@ -8,3 +8,5 @@ Dimostra che questo valore è massimo se si pone P=1/2
 Per trovare il numero medio di partite disputate, possiamo considerare una sequenza di partite fino a quando uno dei due giocatori vince. Sia X il numero di partite disputate fino a quel momento.
 
 La probabilità che la prima partita venga vinta dal primo giocatore è P. In questo caso, il numero medio di partite disputate sarebbe 1, poiché la partita terminerebbe dopo un solo turno.
+
+La probabilità che la prima partita venga vinta dal secondo giocatore è 1 - P. In questo caso, il numero medio di partite disputate sarebbe 1 (la prima partita) più il numero medio di partite rimanenti, che è essenzialmente X, poiché stiamo tornando alla situazione iniziale.
