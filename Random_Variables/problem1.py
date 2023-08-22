71e466d8391b36810c4c3473cc5c9585876387d6
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2023-08-22 07:22:31 +0300

    Update problem1.py

--- Random_Variables/problem1.py
+++ Random_Variables/problem1.py
@@ -4,3 +4,7 @@ vinta(indendentemente dalle altre) dal primo giocatore con probabilità P
 e dal secondo con probabilità 1-P. 
 Trova il numero medio di partite disputyate se i=2. 
 Dimostra che questo valore è massimo se si pone P=1/2
+
+Per trovare il numero medio di partite disputate, possiamo considerare una sequenza di partite fino a quando uno dei due giocatori vince. Sia X il numero di partite disputate fino a quel momento.
+
+La probabilità che la prima partita venga vinta dal primo giocatore è P. In questo caso, il numero medio di partite disputate sarebbe 1, poiché la partita terminerebbe dopo un solo turno.
