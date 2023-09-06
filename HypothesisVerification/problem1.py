e821a6f54a3b59d901c023e3df4238102fecda0d
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2023-09-06 08:02:44 +0200

    Create problem1.py

---
+++ HypothesisVerification/problem1.py
@@ -0,0 +1,6 @@
+'''
+si è pescato un campione di 10 pesci del lago A e 8 del lago B , misurandone la concetrazione di PCB.I valori trovati in parti per milione sono :
+lago A 11.5 10.8 11.6 9.4 12.4 11.4 12.2 11.0 10.6 10.8
+lago B 11.8 12.6 12.2 12.5 11.7 12.1 10.4 12.6
+Sapendo che gli errori statistici di varianza sono 0.09 e 0.16 rispettivamente,si può concludere ad un livello di significatività del 5% che i due laghi sono ugualmente inquinati?
+'''
