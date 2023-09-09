79d088af1685c28e473f2320f5b85787d9f42f17
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2023-09-09 09:31:56 +0200

    Create problem1.py

---
+++ Regression/problem1.py
@@ -0,0 +1,4 @@
+'''
+I dati seguenti mettono in relazione la percentuale di acqua x contenuta in un certo materiale in una delle fasi di lavorazione,con la densità Y del prodotto finito.
+x 5 6 7 10 12 15 18 29
+Y 7.4 9.3 10.6 15.4 18.1 22.2 24.1 24.8
