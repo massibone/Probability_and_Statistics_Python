55802c3034475ad3254d48da1b4fd9b0a49997af
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2023-09-09 09:32:38 +0200

    Update problem1.py

--- Regression/problem1.py
+++ Regression/problem1.py
@@ -2,3 +2,6 @@
 I dati seguenti mettono in relazione la percentuale di acqua x contenuta in un certo materiale in una delle fasi di lavorazione,con la densità Y del prodotto finito.
 x 5 6 7 10 12 15 18 29
 Y 7.4 9.3 10.6 15.4 18.1 22.2 24.1 24.8
+a) traccia il diagramma di dispersione
+b) trova la retta di regressione che interpola questi dati
+'''
