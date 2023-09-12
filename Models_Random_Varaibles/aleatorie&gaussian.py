f3ac8a0aef4d9c6a2b1e141197ce11df3a462ef3
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2023-09-12 08:11:59 +0200

    Update aleatorie&gaussian.py

--- Models_Random_Varaibles/aleatorie&gaussian.py
+++ Models_Random_Varaibles/aleatorie&gaussian.py
@@ -15,3 +15,9 @@ std_dev_diff = 98.99
 probability_diff = 1 - stats.norm.cdf(70, mean_diff, std_dev_diff)
 
 print("La probabilità che le precipitazioni del prossimo anno superino quelle dell'anno successivo per più di 70 mm è:", probability_diff)
+
+'''
+i dati a disposizione di metereologi indicano che le precipitazioni a Firenze hanno distribuzione normale con media 300 mm e deviazione standard 70 m.Assumiamo anche che le precipitazioni di anni successivi siano indipendenti. a)Trova la probabilità che le precipitazioni dei prssimi due anni superino complessivamente i 600 mm b)Trova la probabilità che le precipitazioni del prossimo anno superino quelle dell'anno successivo per piu' di 70 mm 
+Per risolvere questo problema, possiamo utilizzare le proprietà della distribuzione normale.
+
+a) Per calcolare la probabilità che le precipitazioni dei prossimi due anni superino complessivamente i 600 mm, dobbiamo calcolare la probabilità che la somma delle precipitazioni di due anni sia maggiore di 600 mm.
