39b59d8454c5ff2fd683c09cf19553972e6d9632
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2023-09-21 07:16:56 +0200

    Update esponential.py

--- Models_Random_Varaibles/esponential.py
+++ Models_Random_Varaibles/esponential.py
@@ -10,3 +10,17 @@ x = np.linspace(0, 10, 1000)
 
 # Calcolo della funzione di densità di probabilità per la distribuzione esponenziale
 pdf = expon.pdf(x, scale=1/lambda_)
+# Creazione del grafico
+plt.plot(x, pdf, 'r-', lw=2, label='Variabile Aleatoria Esponenziale')
+plt.xlabel('Valore')
+plt.ylabel('Densità di Probabilità')
+plt.title('Funzione di Densità di Probabilità per Variabile Aleatoria Esponenziale')
+plt.legend()
+plt.grid(True)
+
+# Visualizzazione del grafico
+plt.show()
+-gamma.py e png
+-degrreFreedom.py e png
+-problem.py
+
