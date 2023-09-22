19d6ff239d259c51cb13d197feba2efe2c9c4b1f
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2023-09-22 08:00:37 +0200

    Create problem_votante.py

---
+++ Models_Random_Varaibles/problem_votante.py
@@ -0,0 +1,9 @@
+'''
+Se un votante scelto a caso è favorevole ad una certa riforma con probbabilità 0.7,qual'è la probabilità che su 10 votanti esattamente 7 siano favorevoli?
+
+ogni votante ha una probabilità di successo p = 0.7 di essere favorevole alla riforma. Per calcolare la probabilità che esattamente 7 votanti su 10 siano favorevoli, possiamo utilizzare la formula della funzione di massa di probabilità (PMF) della distribuzione binomiale.
+
+La formula della PMF della distribuzione binomiale è:
+
+P(X = k) = C(n, k) * p^k * (1-p)^(n-k)
+'''
