6e525fd1325d4cc76f95357ab0d8c90e6465740f
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2023-09-25 08:08:23 +0200

    Update problem3.py

--- Models_Random_Varaibles/problem3.py
+++ Models_Random_Varaibles/problem3.py
@@ -3,3 +3,11 @@ calcola l'approssimazione della probabilità che la somma dei punteggi di 10 dad
 '''
 import random
 
+def roll_dice():
+    return random.randint(1, 6)
+
+def simulate_dice_rolls(num_rolls):
+    sum_of_scores = 0
+    for _ in range(num_rolls):
+        sum_of_scores += roll_dice()
+    return sum_of_scores
