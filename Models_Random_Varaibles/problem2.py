c4ade5544d4f35264712ed13f4d59b85b2c79af0
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2023-09-26 08:03:13 +0200

    Update problem2.py

--- Models_Random_Varaibles/problem2.py
+++ Models_Random_Varaibles/problem2.py
@@ -1,3 +1,15 @@
 '''
-calcola l'approssimazione della probabilità che la somma dei punteggi ottenuti dal lancio di 10 dadi non superi 400, e determina in maniera approssimativa la probabilità che siano necessari più di 140 lanci.
+calcola l'approssimazione della probabilità che la somma dei punteggi ottenuti dal lancio 
+di 10 dadi non superi 400, 
+e determina in maniera approssimativa la probabilità che siano necessari più di 140 lanci.
 '''
+import random
+
+def roll_dice():
+    return random.randint(1, 6)
+
+def simulate_dice_rolls(num_rolls):
+    sum_of_scores = 0
+    for _ in range(num_rolls):
+        sum_of_scores += roll_dice()
+    return sum_of_scores
