a29ab577d8f2f0517b4ac6a72aef884210faa1ec
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2023-09-26 08:00:49 +0200

    Create problem2.py

---
+++ Models_Random_Varaibles/problem2.py
@@ -0,0 +1,3 @@
+'''
+calcola l'approssimazione della probabilità che la somma dei punteggi ottenuti dal lancio di 10 dadi non superi 400, e determina in maniera approssimativa la probabilità che siano necessari più di 140 lanci.
+'''
