90c8a504b260c8e4b0515a9915485211241260a1
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2023-09-26 08:06:59 +0200

    Update problem2.py

--- Models_Random_Varaibles/problem2.py
+++ Models_Random_Varaibles/problem2.py
@@ -13,3 +13,22 @@ def simulate_dice_rolls(num_rolls):
     for _ in range(num_rolls):
         sum_of_scores += roll_dice()
     return sum_of_scores
+def calculate_probability(num_simulations, target_sum):
+    count_successful = 0
+    num_rolls = 0
+    while count_successful < num_simulations:
+        num_rolls += 1
+        sum_of_scores = simulate_dice_rolls(10)
+        if sum_of_scores <= target_sum:
+            count_successful += 1
+    probability = count_successful / num_simulations
+    return probability
+
+num_simulations = 1000  # Numero di simulazioni da eseguire
+target_sum = 400  # Soglia della somma dei punteggi
+
+probability = calculate_probability(num_simulations, target_sum)
+print(f"Probabilità approssimata: {probability}")
+'''
+il punteggio massimo ottenibile con 10 dadi è 60 (se tutti i dadi mostrano 6). Pertanto, la probabilità che siano necessari più di 140 lanci sarà 0.
+'''
