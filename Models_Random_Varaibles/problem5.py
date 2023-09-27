d4395d7048a3d72c3900fae1ca0381ef3cda086c
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2023-09-27 08:00:29 +0200

    Update problem5.py

--- Models_Random_Varaibles/problem5.py
+++ Models_Random_Varaibles/problem5.py
@@ -4,3 +4,21 @@ La distribuzione binomiale viene utilizzata per modellare eventi binari (success
 
 Nel tuo caso, la probabilità di successo (essere mancini) è del 12% o 0.12
 '''
+
+from scipy.stats import binom
+
+def calculate_probability(sample_size, p, lower_bound, upper_bound):
+    probability = 0
+    for i in range(lower_bound, upper_bound + 1):
+        probability += binom.pmf(i, sample_size, p)
+    return probability
+
+sample_size = 100
+p = 0.12
+lower_bound = 10
+upper_bound = 12
+
+probability = calculate_probability(sample_size, p, lower_bound, upper_bound)
+print(f"Probabilità: {probability}")
+
+# probabilità del 35%
