4a3c01a1a6b35ff3e4c3b97c9744e956c9818204
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2023-09-27 07:59:39 +0200

    Create problem5.py

---
+++ Models_Random_Varaibles/problem5.py
@@ -0,0 +1,6 @@
+'''
+l 12% della popolazione mondiale è mancina.,trova la probabilità che in un campione aleatorio di 100 persone vi siano persone mancine il numero e che vi sia un numero di mancini tra 10 e 12.
+La distribuzione binomiale viene utilizzata per modellare eventi binari (successo/fallimento) ripetuti in modo indipendente.
+
+Nel tuo caso, la probabilità di successo (essere mancini) è del 12% o 0.12
+'''
