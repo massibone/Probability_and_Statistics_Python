047a4d2e1e4bfb1b8d6900cb8cf210431bac0d0f
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2023-09-28 08:02:10 +0200

    Update problem6.py

--- Models_Random_Varaibles/problem6.py
+++ Models_Random_Varaibles/problem6.py
@@ -1,1 +1,4 @@
-
+'''
+a media e la deviazione standard dei punteggi della prova di matematica di tutte le scuole superiori di Milano sono stati 517 e 120.
+Trova la probabilità che un campione di 144 studenti abbia un punteggio che superi: a)507 b) 517c)537 d)550
+'''
