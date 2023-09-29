3476805789d1236e846b4dfd0c12922d05733068
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2023-09-29 08:07:57 +0200

    Update problem1.py

--- ParametricEstimation/problem1.py
+++ ParametricEstimation/problem1.py
@@ -10,3 +10,20 @@ Dove t è il valore critico corrispondente al livello di confidenza desiderato, 
 Per un intervallo di confidenza bilaterale al 99%, il livello di confidenza è del 99% / 2 = 0.995. Utilizzando una tabella o una funzione di distribuzione t, troviamo che il valore critico corrispondente a un livello di confidenza di 0.995 con 19 gradi di libertà (20 - 1) è di circa 2.861.
 '''
 
+import math
+
+sample_mean = 1.2  # Valore medio del campione
+sample_std_dev = 0.2  # Deviazione standard del campione
+sample_size = 20  # Dimensione del campione
+
+confidence_level = 0.99  # Livello di confidenza bilaterale
+
+critical_value = 2.861  # Valore critico t corrispondente al livello di confidenza
+
+standard_error = sample_std_dev / math.sqrt(sample_size)  # Errore standard
+
+lower_bound = sample_mean - (critical_value * standard_error)
+upper_bound = sample_mean + (critical_value * standard_error)
+
+print(f"Intervallo di confidenza al {confidence_level * 100}%: [{lower_bound}, {upper_bound}]")
+#Intervallo di confidenza al 99.0%: [1.072052190327462, 1.327947809672538]
