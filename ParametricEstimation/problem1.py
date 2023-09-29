b10f5a4762118ab13eb1ac62f91a45af632464dc
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2023-09-29 08:07:21 +0200

    Create problem1.py

---
+++ ParametricEstimation/problem1.py
@@ -0,0 +1,12 @@
+'''
+Si analizza un campione di 20 sigarette per vedere contenuto nicotina e il valore medio è di 1.2mg.calcola un intervallo di confidenza  bilaterale al 99% per il contenuto medio di nicotina di quel tipo di sigarette sapendo che la deviazione standard è 0.2mg.
+
+L'intervallo di confidenza può essere calcolato utilizzando la formula:
+
+Intervallo di confidenza = valore medio ± (t * errore standard)
+
+Dove t è il valore critico corrispondente al livello di confidenza desiderato, errore standard è la deviazione standard campionaria divisa per la radice quadrata della dimensione del campione.
+
+Per un intervallo di confidenza bilaterale al 99%, il livello di confidenza è del 99% / 2 = 0.995. Utilizzando una tabella o una funzione di distribuzione t, troviamo che il valore critico corrispondente a un livello di confidenza di 0.995 con 19 gradi di libertà (20 - 1) è di circa 2.861.
+'''
+
