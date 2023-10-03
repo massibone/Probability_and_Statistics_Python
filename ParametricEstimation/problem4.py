1470f29bead994182690a26edbadf8e3aa7c8e6f
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2023-10-03 08:05:56 +0200

    Update problem4.py

--- ParametricEstimation/problem4.py
+++ ParametricEstimation/problem4.py
@@ -1,1 +1,5 @@
+'''
+un campione casuale di 1200 ingegneri è risultato ottentere 48 ispanoamericani 60 afroamericani e 204 femmine.determina gli intervalli di confidenza al 90% per la frazione di ingegneri che sono a)femmine b)ispanoamericani c)afroamericani
+'''
+
 
