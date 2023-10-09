3fea5549e5dc5013d3a257339025389d248ec0cd
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2023-10-09 08:04:53 +0200

    Create problem1.py

---
+++ ANOVA/problem1.py
@@ -0,0 +1,8 @@
+'''Si scelgono 20 persone sovrappeso e ciascuno viene sottoposto a dieta.Dopo 10 settimane le diminuzioni di peso sono state
+dieta1  22.2 23.4 24.2 16.1 9.4 12.5 18.6 32.2 8.8 7.6
+dieta2 24.2 16.8 14.6 13.7 19.5 17.6 11.2 9.5 30.1 21.5
+Verifica al 5% di significatività l'ipotesi che le due diete abbiano uguale effetto.
+
+possiamo utilizzare un test di confronto tra due medie noto come "t-test" per campioni indipendenti. Poiché si tratta di un test bilaterale e la dimensione del campione è piccola, useremo il t-test per campioni indipendenti con un livello di significatività del 5%.
+
+'''
