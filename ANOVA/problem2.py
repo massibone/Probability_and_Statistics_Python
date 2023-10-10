395f15c08557e2040aeba9fa33488249b7cc2602
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2023-10-10 08:00:15 +0200

    Create problem2.py

---
+++ ANOVA/problem2.py
@@ -0,0 +1,9 @@
+'''
+verifica che questi tre campioni indipendent provengano tutti dalla stessa polozaione normale.
+Test di normalità. Un test comunemente usato per questo scopo è il test di Shapiro-Wilk.
+camp1 35 37 29 27 30
+camp2 29 38 34 30 32 
+camp3 44 52 56
+-------
+Se il valore di p ottenuto per ciascun campione è superiore al livello di significatività desiderato (ad esempio, 0.05), possiamo concludere che non abbiamo sufficienti evidenze per rigettare l'ipotesi nulla e accettare che i dati provengono da una distribuzione normale comune. Altrimenti, se il valore di p è inferiore al livello di significatività, dovremmo rigettare l'ipotesi nulla e considerare i dati non provenienti da una distribuzione normale comune.
+'''
