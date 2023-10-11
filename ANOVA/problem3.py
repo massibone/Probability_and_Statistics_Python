62a861d9541483a669eb9da82a539e8cb6cedb7e
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2023-10-11 08:00:46 +0200

    Create problem3.py

---
+++ ANOVA/problem3.py
@@ -0,0 +1,9 @@
+'''
+abbiamo due produttori da scegliere. li sottoponiamo a 8 problemi di calcolo e misura dei tempi necessari ai 2 calcolatori per risolverli
+Problema  1      2     3     4     5    6    7     8
+sistemaA  15   32   17   26   42 29 12   38
+sistemaB  22  29   1     23    46 25  19  47
+Determina il p-dei-dati del test e l'ipotesi nulla che nn vi siano differenze.
+Per verificare se ci sono differenze significative tra i due produttori, possiamo utilizzare un test statistico chiamato test t per campioni indipendenti. In questo caso, il test t ci aiuterà a confrontare i tempi necessari per risolvere i problemi nei due sistemi.
+
+'''
