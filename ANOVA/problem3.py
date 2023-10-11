ec7af6d5bf8a509fdb2864eaba6a7477047572e2
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2023-10-11 08:01:23 +0200

    Update problem3.py

--- ANOVA/problem3.py
+++ ANOVA/problem3.py
@@ -7,3 +7,16 @@ Determina il p-dei-dati del test e l'ipotesi nulla che nn vi siano differenze.
 Per verificare se ci sono differenze significative tra i due produttori, possiamo utilizzare un test statistico chiamato test t per campioni indipendenti. In questo caso, il test t ci aiuterà a confrontare i tempi necessari per risolvere i problemi nei due sistemi.
 
 '''
+import numpy as np
+from scipy.stats import ttest_ind
+
+# Tempi necessari ai due produttori per risolvere i problemi
+sistemaA = np.array([15, 32, 17, 26, 42, 29, 12, 38])
+sistemaB = np.array([22, 29, 1, 23, 46, 25, 19, 47])
+
+# Test t per campioni indipendenti
+t_statistic, p_value = ttest_ind(sistemaA, sistemaB)
+
+# Stampa i risultati
+print("Valore della statistica t:", t_statistic)
+print("Valore di p:", p_value)
