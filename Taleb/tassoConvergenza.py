c764b2630072a3c5d1b8bcde8243d345deb1abbe
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2024-03-25 08:05:17 +0100

    Update tassoConvergenza.py

--- Taleb/tassoConvergenza.py
+++ Taleb/tassoConvergenza.py
@@ -1,1 +1,9 @@
+'''
+il tasso di convergenza di una distribuzione Student T alla zona gaussiana. 
+Si evidenzia che la convergenza dell'indice κ a 0 sotto sommatoria è di 1/log(n). 
+Viene menzionata la forma semi-chiusa per la densità di una distribuzione Student cubica sommata n volte. Inoltre, 
+si parla dell'approccio di Bouchaud e Potters che separa la "zona gaussiana" dalla "zona Power Law" nelle code della distribuzione. 
+Si sottolinea che il passaggio tra le due zone avviene a una velocità molto lenta. 
+Infine, si menziona l'interesse per la convergenza della Pareto a una distribuzione Levy-Stable, che finora è stata ottenuta solo numericamente.
+'''
 
