01f52864acb255bfb76e154294d7a83ab990381d
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2024-04-06 11:16:26 +0200

    Update indicatoriK.py

--- Taleb/indicatoriK.py
+++ Taleb/indicatoriK.py
@@ -1,1 +1,3 @@
+'''
+Immagina di avere una scatola piena di soldi, e ogni volta che metti più soldi dentro, la scatola diventa più grande. Ora, c'è una cosa speciale che stiamo guardando, chiamata "κbq", che ci dice quanto sia grande una scatola speciale che usiamo per misurare quanta ricchezza c'è in giro. Quello che abbiamo notato è che se la nostra scatola speciale diventa più grande, c'è più probabilità che metteremo più soldi dentro. È come se più soldi ci sono, più la nostra scatola speciale diventa grande.
 
