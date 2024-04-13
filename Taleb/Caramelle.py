1a13dc6ae4fbc63ce021e2ae390e561bc65c7e4e
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2024-04-13 11:13:30 +0200

    Update Caramelle.py

--- Taleb/Caramelle.py
+++ Taleb/Caramelle.py
@@ -1,1 +1,5 @@
+'''
+Immagina di avere una grande ciotola di caramelle, ma molte di queste caramelle sono raccolte in pochi posti anziché essere distribuite uniformemente in tutta la ciotola. Questo è un esempio di concentrazione.
+La concentrazione può sembrare alta se guardiamo l'intera ciotola, ma se guardiamo solo piccole parti della ciotola, potremmo vedere una concentrazione più bassa. Questo è come guardare una foto da lontano e vedere molte caramelle insieme, ma se ti avvicini e guardi da vicino, vedrai che le caramelle sono sparse in modo diverso.
+'''
 
