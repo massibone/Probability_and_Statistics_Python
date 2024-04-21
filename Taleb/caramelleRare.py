35d0eed5842a17504f241774bc61936298e75343
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2024-04-21 12:31:24 +0200

    Update caramelleRare.py

--- Taleb/caramelleRare.py
+++ Taleb/caramelleRare.py
@@ -1,1 +1,8 @@
+'''
+ho simulato una lista di quantità di caramelle rare e ho calcolato una stima del totale del mucchio basata su queste quantità simulate.
+'''
+import random
+
+# Generazione casuale di una lista di quantità di caramelle (simula dati di caramelle rare)
+quantita_caramelle_rare = [random.randint(1, 100) for _ in range(50)]
 
