2ebb562b55962adad0b7e294d39efca1955f3b55
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2024-04-27 15:18:32 +0200

    Update missingEvents.py

--- Taleb/missingEvents.py
+++ Taleb/missingEvents.py
@@ -1,1 +1,4 @@
+'''
+simula l'effetto di rimuovere una proporzione specifica di eventi da un campione di eventi storici. La simulazione calcola quanti eventi vengono rimossi, quanti ne rimangono e la proporzione degli eventi rimossi rispetto al totale degli eventi.
+'''
 
