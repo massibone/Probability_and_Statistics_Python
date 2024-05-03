024eb7ae672c58ec6ed1204a5fc2f8eeb14de321
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2024-05-03 08:01:27 +0200

    Update es1.py

--- Taleb/es1.py
+++ Taleb/es1.py
@@ -1,1 +1,10 @@
+'''
+Immagina di avere un grosso barattolo pieno di molte piccole pietre di diversi colori. Ogni pietra rappresenta un numero casuale diverso. Se prendi un mucchio di queste pietre e sommi insieme i loro numeri, otterrai un numero che potrebbe sembrare simile a una forma speciale chiamata distribuzione normale o "a campana".
+
+Ora, immagina di voler fare qualcosa di diverso. Invece di avere solo pietre con numeri precisi, aggiungiamo un po' di confusione o incertezza. Potremmo prendere una manciata di queste pietre e dare loro numeri che possono cambiare leggermente o essere meno precisi. Questo rende la distribuzione meno regolare e può far sì che i numeri risultanti siano più variabili o "grassi" nei bordi.
+
+Quindi, quello che stiamo facendo è aggiungere sempre più livelli di incertezza. Non ci fermiamo a una sola volta, ma continuiamo ad aggiungere incertezza su incertezza. È come mettere dentro una scatola una scatola più piccola, e poi una ancora più piccola, e così via.
+
+Questo modo di aggiungere incertezza può aiutarci a capire meglio come certe cose si comportano quando diventano più complesse, senza dover usare solo i numeri. È come una sorta di gioco di costruzione concettuale, dove possiamo esplorare idee strane e complesse.
+'''
 
