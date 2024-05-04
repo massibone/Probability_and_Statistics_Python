c4b0c68547c3a862f4505ea87b28235178df9351
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2024-05-04 08:28:38 +0200

    Update Livelli_incertezza.py

--- Taleb/Livelli_incertezza.py
+++ Taleb/Livelli_incertezza.py
@@ -1,1 +1,4 @@
 
+'''
+Immagina di avere un vaso con caramelle rosse e blu, e vuoi sapere quante sono rosse. Ma non sei sicuro della precisione del tuo conteggio. Quindi, anziché dire esattamente quante caramelle rosse ci sono, diciamo che potrebbe esserci un po' di incertezza nel conteggio. Inoltre, non sei sicuro di quanto sei sicuro nel tuo conteggio impreciso!
+'''
