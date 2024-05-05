4ef4c06234f339abc1e57d50a17567949fd992c9
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2024-05-05 07:42:31 +0200

    Update Livelli_incertezza.py

--- Taleb/Livelli_incertezza.py
+++ Taleb/Livelli_incertezza.py
@@ -2,3 +2,10 @@
 '''
 Immagina di avere un vaso con caramelle rosse e blu, e vuoi sapere quante sono rosse. Ma non sei sicuro della precisione del tuo conteggio. Quindi, anziché dire esattamente quante caramelle rosse ci sono, diciamo che potrebbe esserci un po' di incertezza nel conteggio. Inoltre, non sei sicuro di quanto sei sicuro nel tuo conteggio impreciso!
 '''
+import random
+
+# Funzione per calcolare il numero approssimato di caramelle rosse con incertezza
+def calcola_caramelle_rosse():
+    # Numero totale di caramelle nel vaso
+    numero_totale_caramelle = 100
+    
