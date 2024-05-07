ad395f24de1fe097b5a31be6b4260c7c3d9dcb84
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2024-05-07 08:01:30 +0200

    Update classify_toys.py

--- Taleb/classify_toys.py
+++ Taleb/classify_toys.py
@@ -1,1 +1,4 @@
-
+'''
+determine if a toy is "big" or "small" based on whether it's an even or odd number. 
+This represents a basic concept of machine learning - learning rules from data (toy sizes) to make decisions (classification).
+'''
