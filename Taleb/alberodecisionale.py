23842cb00692b0b7588b6c2cbb8384f4e5f30ef9
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2024-05-08 08:05:11 +0200

    Update alberodecisionale.py

--- Taleb/alberodecisionale.py
+++ Taleb/alberodecisionale.py
@@ -1,1 +1,5 @@
+'''
+Immagina di avere una scatola di mattoncini LEGO. Vorresti costruire una macchina che possa raccogliere tutti i mattoncini gialli sparsi nella stanza. Ma non vuoi dirle esattamente cosa fare. Quindi, la prima volta, la guidi mostrandole come raccogliere i mattoncini gialli.
 
+La prossima volta, la macchina proverà a raccogliere i mattoncini gialli da sola, guardando ciò che hai fatto la volta precedente. Se sbaglia, impara e prova di nuovo fino a quando non riesce a farlo da sola senza il tuo aiuto. Questo è un po' come il machine learning, dove il computer impara dagli esempi e diventa sempre più bravo nel fare cose da solo.
+'''
