1c812bba42bddcd64500fcc01aab656115120a9a
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2024-05-12 00:14:01 +0200

    Create Immagine.py

---
+++ Taleb/Immagine.py
@@ -0,0 +1,3 @@
+'''
+Immagina di lanciare un dado molte volte e ogni volta ottenere un risultato diverso. I risultati potrebbero sembrare strani e volatili, ma il testo cerca di capire come le distribuzioni di questi risultati cambino quando si eseguono gli stessi esperimenti molte volte.
+'''
