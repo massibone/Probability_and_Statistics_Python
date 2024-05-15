1417e121f9231f0e3d531c0686bc43a83ab2d2e8
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2024-05-15 12:41:39 +0200

    Update premioEquita.py

--- Taleb/premioEquita.py
+++ Taleb/premioEquita.py
@@ -1,1 +1,5 @@
+'''
+Il "puzzle del premio di equità" si riferisce al fenomeno per cui le azioni hanno storicamente reso un rendimento più elevato rispetto agli investimenti a reddito fisso, senza che questo eccesso di rendimento sia stato arbitrato via nel tempo. Questo studio mette in discussione l'analisi di tale fenomeno, sottolineando l'importanza di considerare gli effetti di secondo ordine, come la mancanza di ergodicità e le code spesse nella distribuzione delle variabili finanziarie.
+'''
+
 
