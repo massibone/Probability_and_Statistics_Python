227832c5042049c317f56a245f62263c2eaff4f9
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2024-05-19 01:01:35 +0200

    Update opzioniEuropee.py

--- Taleb/opzioniEuropee.py
+++ Taleb/opzioniEuropee.py
@@ -9,3 +9,9 @@ def european_put_option(S0, K, t, r, T):
     F = S0 * np.exp(r * (T - t))
     return max(K - F, 0)
 
+# Definizione dei parametri
+S0 = 100  # Prezzo attuale dell'attività sottostante
+K = 110  # Prezzo di esercizio dell'opzione
+t = 0  # Tempo attuale
+r = 0.05  # Tasso di interesse senza rischio
+T = 1  # Tempo di scadenza dell'opzione
