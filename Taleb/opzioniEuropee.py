d63f8419b96ce06c3e89837feb96c85afccc2797
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2024-05-18 09:27:26 +0200

    Update opzioniEuropee.py

--- Taleb/opzioniEuropee.py
+++ Taleb/opzioniEuropee.py
@@ -1,1 +1,11 @@
+import numpy as np
+
+# Definizione delle funzioni per il calcolo delle opzioni di put e call
+def european_call_option(S0, K, t, r, T):
+    F = S0 * np.exp(r * (T - t))
+    return max(F - K, 0)
+
+def european_put_option(S0, K, t, r, T):
+    F = S0 * np.exp(r * (T - t))
+    return max(K - F, 0)
 
