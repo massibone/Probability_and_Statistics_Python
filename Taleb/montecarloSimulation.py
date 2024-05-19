9bb59e32b34103463f7c721020211b5d24ba0701
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2024-05-19 23:38:09 +0200

    Update montecarloSimulation.py

--- Taleb/montecarloSimulation.py
+++ Taleb/montecarloSimulation.py
@@ -1,6 +1,7 @@
 '''
 simula il processo di generazione di valori casuali
-uniformemente distribuiti tra un limite inferiore e uno superiore per un evento specifico. 
+uniformemente distribuiti tra un limite inferiore e 
+uno superiore per un evento specifico. 
 '''
 
 
