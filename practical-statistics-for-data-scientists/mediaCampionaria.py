f9e75ce47ef1f55dab8e526152296647ec90a80c
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2024-06-12 08:51:44 +0200

    Create mediaCampionaria.py

---
+++ practical-statistics-for-data-scientists/mediaCampionaria.py
@@ -0,0 +1,3 @@
+'''
+Immagina di avere una grande collezione di conchiglie di mare. Ogni volta che prendi un piccolo gruppo di conchiglie e ne calcoli la media delle dimensioni, stai essenzialmente facendo quello che il teorema del limite centrale descrive. Anche se i tuoi piccoli gruppi di conchiglie potrebbero variare, se prendi abbastanza gruppi e calcoli le loro medie, alla fine assomiglieranno a una forma a campana come una montagna rotonda. È come se ogni gruppo di conchiglie contribuisse un pezzo alla forma della montagna.
+'''
