1d821bfc2448aae06c3287bb7393adcdba623d99
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2024-09-26 08:23:40 +0200

    Update qqplot.py

--- practical-statistics-for-data-scientists/qqplot.py
+++ practical-statistics-for-data-scientists/qqplot.py
@@ -1,1 +1,4 @@
+'''
+Una distribuzione normale standard è una distribuzione normale dove i valori sull'asse x sono espressi in termini di deviazioni standard dalla media. Per confrontare i dati con una distribuzione normale standard, si sottrae la media e si divide per la deviazione standard, un processo noto come standardizzazione o normalizzazione, ottenendo così i cosiddetti z-score. Un QQ-Plot è utilizzato per confrontare visivamente una distribuzione campionaria con una distribuzione specificata, come quella normale. Se i punti del QQ-Plot cadono approssimativamente sulla linea diagonale, significa che la distribuzione del campione è vicina a quella normale.
+'''
 
