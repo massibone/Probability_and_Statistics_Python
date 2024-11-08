bc85e56f24729dc6b5a1e8aae912afe09bc5baf7
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2024-11-08 08:05:34 +0100

    Update long-tailed-distributions.py

--- practical-statistics-for-data-scientists/long-tailed-distributions.py
+++ practical-statistics-for-data-scientists/long-tailed-distributions.py
@@ -1,1 +1,3 @@
-
+'''
+Le distribuzioni con code lunghe (long-tailed distributions) sono distribuzioni in cui esistono valori estremi (molto grandi o molto piccoli) che si verificano con bassa frequenza. Anche se la distribuzione normale è storicamente importante nelle statistiche, molte volte i dati reali non seguono una distribuzione normale. La distribuzione può essere altamente asimmetrica (skewed) o discreta. Le code lunghe sono comuni nei dati pratici, come i ritorni azionari, dove eventi estremi accadono più frequentemente di quanto predetto dalla distribuzione normale.
+'''
