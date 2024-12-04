6cd7fdd9e3d66e4939e26d5eb289bc6900e7895e
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2024-12-04 08:06:00 +0100

    Update chi-quadrato.py

--- practical-statistics-for-data-scientists/chi-quadrato.py
+++ practical-statistics-for-data-scientists/chi-quadrato.py
@@ -1,5 +1,7 @@
 '''
-Immagina di avere un gioco in cui devi lanciare un dado e vedere quante volte esce ciascun numero. Ora, diciamo che ti aspetti che ogni numero esca un numero uguale di volte, ma quando lanci il dado, ottieni risultati diversi. Il chi-quadrato è come una misura di quanto i tuoi risultati sono diversi da ciò che ti aspettavi. Se i risultati sono molto diversi, il valore del chi-quadrato sarà alto.
+Immagina di avere un gioco in cui devi lanciare un dado e vedere quante volte esce ciascun numero. 
+Ora, diciamo che ti aspetti che ogni numero esca un numero uguale di volte, ma quando lanci il dado, ottieni risultati diversi. 
+Il chi-quadrato è come una misura di quanto i tuoi risultati sono diversi da ciò che ti aspettavi. Se i risultati sono molto diversi, il valore del chi-quadrato sarà alto.
 '''
 import numpy as np
 from scipy.stats import chi2_contingency
