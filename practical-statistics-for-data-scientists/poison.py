e3d6c18da339c2cfdf0ee23999c8b69461196ea6
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2024-12-05 08:08:09 +0100

    Update poison.py

--- practical-statistics-for-data-scientists/poison.py
+++ practical-statistics-for-data-scientists/poison.py
@@ -1,1 +1,3 @@
-
+'''
+Immagina di avere diversi gruppi di piante in un campo e stai cercando di capire se un certo fertilizzante fa crescere le piante meglio di un altro. La distribuzione F è come una misura che ci dice se le differenze tra i gruppi di piante sono grandi rispetto alle differenze che ci aspetteremmo solo per caso. Se la differenza è grande, la distribuzione F ci darà un valore alto.
+'''
