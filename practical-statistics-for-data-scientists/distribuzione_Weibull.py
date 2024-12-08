105f4d706deeee93cdc220207dd58cb152a8fc4d
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2024-12-08 18:02:13 +0100

    Update distribuzione_Weibull.py

--- practical-statistics-for-data-scientists/distribuzione_Weibull.py
+++ practical-statistics-for-data-scientists/distribuzione_Weibull.py
@@ -1,5 +1,6 @@
 '''
-La distribuzione di Weibull è usata per modellare eventi il cui tasso cambia nel tempo, come il fallimento meccanico, dove il rischio di fallimento aumenta con il passare del tempo. Ha due parametri: β (shape) e η (scale). Se β > 1, la probabilità di un evento aumenta nel tempo; se β < 1, la probabilità diminuisce. La caratteristica vita (η) rappresenta il tempo caratteristico al posto del tasso degli eventi. La distribuzione di Weibull è quindi utile per l'analisi del tempo di fallimento. In Python, si possono generare numeri casuali da una distribuzione di Weibull usando stats.weibull_min.rvs.
+La distribuzione di Weibull 
+è usata per modellare eventi il cui tasso cambia nel tempo, come il fallimento meccanico, dove il rischio di fallimento aumenta con il passare del tempo. Ha due parametri: β (shape) e η (scale). Se β > 1, la probabilità di un evento aumenta nel tempo; se β < 1, la probabilità diminuisce. La caratteristica vita (η) rappresenta il tempo caratteristico al posto del tasso degli eventi. La distribuzione di Weibull è quindi utile per l'analisi del tempo di fallimento. In Python, si possono generare numeri casuali da una distribuzione di Weibull usando stats.weibull_min.rvs.
 '''
 import numpy as np
 import matplotlib.pyplot as plt
