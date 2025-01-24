dcb9676a1feddd689d7ea228a15c118ca937360d
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2025-01-24 08:14:52 +0100

    Update Multi-Arm_Bandit.py

--- practical-statistics-for-data-scientists/Multi-Arm_Bandit.py
+++ practical-statistics-for-data-scientists/Multi-Arm_Bandit.py
@@ -1,3 +1,8 @@
 '''
-Gli A/B test sono popolari nel marketing e nel commercio elettronico, ma non sono l'unico tipo di esperimento statistico. È possibile includere trattamenti aggiuntivi e prendere misure ripetute sui soggetti. I trial farmaceutici, ad esempio, possono avere diverse opportunità per fermare l'esperimento e raggiungere una conclusione. I design sperimentali tradizionali rispondono a domande statiche sull'efficacia dei trattamenti specificati, ma i data scientist sono più interessati a sapere quale, tra molteplici opzioni, è la migliore.
+Gli A/B test sono popolari nel marketing e nel commercio elettronico, 
+ma non sono l'unico tipo di esperimento statistico. 
+È possibile includere trattamenti aggiuntivi e prendere misure ripetute sui soggetti. 
+I trial farmaceutici, ad esempio, possono avere diverse opportunità per fermare l'esperimento e raggiungere una conclusione. 
+I design sperimentali tradizionali rispondono a domande statiche sull'efficacia dei trattamenti specificati, 
+ma i data scientist sono più interessati a sapere quale, tra molteplici opzioni, è la migliore.
 
