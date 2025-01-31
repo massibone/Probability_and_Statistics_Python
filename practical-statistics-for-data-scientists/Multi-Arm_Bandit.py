562f647ff099f0fd2611c60fdce8ba0f5866c56b
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2025-01-31 08:04:32 +0100

    Update Multi-Arm_Bandit.py

--- practical-statistics-for-data-scientists/Multi-Arm_Bandit.py
+++ practical-statistics-for-data-scientists/Multi-Arm_Bandit.py
@@ -2,9 +2,12 @@
 Gli A/B test sono popolari nel marketing e nel commercio elettronico, 
 ma non sono l'unico tipo di esperimento statistico. 
 È possibile includere trattamenti aggiuntivi e prendere misure ripetute sui soggetti. 
-I trial farmaceutici, ad esempio, possono avere diverse opportunità per fermare l'esperimento e raggiungere una conclusione. 
-I design sperimentali tradizionali rispondono a domande statiche sull'efficacia dei trattamenti specificati, 
-ma i data scientist sono più interessati a sapere quale, tra molteplici opzioni, è la migliore.
+I trial farmaceutici, ad esempio, 
+possono avere diverse opportunità per fermare l'esperimento e raggiungere una conclusione. 
+I design sperimentali tradizionali rispondono a domande statiche 
+sull'efficacia dei trattamenti specificati, 
+ma i data scientist sono più interessati a sapere quale, 
+tra molteplici opzioni, è la migliore.
 
 Per questo motivo, viene utilizzato un nuovo tipo di design 
 sperimentale: l'algoritmo multi-armed bandit. 
