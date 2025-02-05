fc1f90149c8ead5acfd49e4db73160207c3b7ba7
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2025-02-05 08:11:36 +0100

    Update test_uni_bi_direzionale.py

--- practical-statistics-for-data-scientists/test_uni_bi_direzionale.py
+++ practical-statistics-for-data-scientists/test_uni_bi_direzionale.py
@@ -5,3 +5,7 @@ Questo test considera solo i risultati estremi in una direzione per calcolare il
 Se invece vuoi proteggerti dal rischio di essere ingannato dal caso in entrambe le direzioni 
 (A è diverso da B, sia maggiore che minore), utilizzi un test di ipotesi bidirezionale (two-tail). 
 Questo test considera i risultati estremi in entrambe le direzioni per calcolare il valore p.
+
+Il test unidirezionale è spesso più adatto per le decisioni A/B, 
+ma molti statistici preferiscono il test bidirezionale per evitare discussioni. 
+Il software di analisi, come R e scipy in Python, fornisce spesso un test bidirezionale di default.
