8c1e88fa7f6913018d689eb976816cf85279f24d
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2025-02-05 08:10:55 +0100

    Update test_uni_bi_direzionale.py

--- practical-statistics-for-data-scientists/test_uni_bi_direzionale.py
+++ practical-statistics-for-data-scientists/test_uni_bi_direzionale.py
@@ -2,3 +2,4 @@
 In un test A/B, quando confronti una nuova opzione (B) con una opzione predefinita (A), 
 potresti voler utilizzare un test di ipotesi unidirezionale (one-tail) se ti interessa solo dimostrare che B è migliore di A. 
 Questo test considera solo i risultati estremi in una direzione per calcolare il valore p.
+Se invece vuoi proteggerti dal rischio di essere ingannato dal caso in entrambe le direzioni (A è diverso da B, sia maggiore che minore), utilizzi un test di ipotesi bidirezionale (two-tail). Questo test considera i risultati estremi in entrambe le direzioni per calcolare il valore p.
