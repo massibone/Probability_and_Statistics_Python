0551568235036878b1ab968d86f8b9606487682e
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2025-02-10 08:07:12 +0100

    Update test_uni_bi_direzionale.py

--- practical-statistics-for-data-scientists/test_uni_bi_direzionale.py
+++ practical-statistics-for-data-scientists/test_uni_bi_direzionale.py
@@ -17,6 +17,7 @@ from scipy import stats
 np.random.seed(42)  # Per riproducibilità
 group_A = np.random.normal(100, 10, 30)
 group_B = np.random.normal(105, 10, 30)
+
 # Test bidirezionale (two-tail)
 t_statistic, p_value_two_tail = stats.ttest_ind(group_A, group_B)
 print("Test bidirezionale:")
