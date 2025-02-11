4946e872023f458402939ddc5380817be422db82
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2025-02-11 10:37:39 +0100

    Create significativita.py

---
+++ practical-statistics-for-data-scientists/significativita.py
@@ -0,0 +1,3 @@
+'''
+La significatività statistica misura se un risultato di un esperimento è più estremo di quanto la casualità potrebbe produrre. In un esempio di test sul web, due prezzi mostrano tassi di conversione diversi. Anche con oltre 45.000 dati, è importante testare la significatività statistica a causa dei bassi tassi di conversione (meno dell'1%). Usando una procedura di permutazione, possiamo determinare se la differenza osservata è dovuta al caso o è significativa.
+'''
