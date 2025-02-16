d3ffe5c43313bf88038f9589357dfa8df3491222
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2025-02-16 08:29:07 +0100

    Update significativita.py

--- practical-statistics-for-data-scientists/significativita.py
+++ practical-statistics-for-data-scientists/significativita.py
@@ -30,3 +30,6 @@ def perm_fun(x, nA, nB):
     idx_B = set(random.sample(range(n), nB))
     idx_A = set(range(n)) - idx_B
     return x.iloc[list(idx_B)].mean() - x.iloc[list(idx_A)].mean()
+# Numero di campionamenti
+nA = conversion_A + no_conversion_A
+nB = conversion_B + no_conversion_B
