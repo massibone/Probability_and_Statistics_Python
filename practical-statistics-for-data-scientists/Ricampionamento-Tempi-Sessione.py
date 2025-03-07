87557ae46eaba91c21a14af1a4627725410032f6
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2025-03-07 08:10:58 +0100

    Update Ricampionamento-Tempi-Sessione.py

--- practical-statistics-for-data-scientists/Ricampionamento-Tempi-Sessione.py
+++ practical-statistics-for-data-scientists/Ricampionamento-Tempi-Sessione.py
@@ -9,3 +9,12 @@ def load_session_data(file_path):
     session_times = pd.read_csv(file_path)
     session_times.Time = 100 * session_times.Time
     return session_times
+
+def plot_boxplot(data):
+    """Crea un boxplot dei tempi di sessione per pagina"""
+    ax = data.boxplot(by='Page', column='Time', figsize=(4, 4))
+    ax.set_xlabel('')
+    ax.set_ylabel('Time (in seconds)')
+    plt.suptitle('')
+    plt.tight_layout()
+    plt.show()
