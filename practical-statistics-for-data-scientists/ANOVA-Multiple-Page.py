4ac3edb6547fe8c9365028870ca19955e9a00ead
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2025-03-27 08:07:14 +0100

    Update ANOVA-Multiple-Page.py

--- practical-statistics-for-data-scientists/ANOVA-Multiple-Page.py
+++ practical-statistics-for-data-scientists/ANOVA-Multiple-Page.py
@@ -1,1 +1,7 @@
-
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import random
+from scipy import stats
+import statsmodels.api as sm
+import statsmodels.formula.api as smf
