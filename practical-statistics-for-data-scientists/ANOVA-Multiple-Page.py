27b3f41635da14198528a9855431bbb0c922b50f
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2025-03-27 08:07:55 +0100

    Update ANOVA-Multiple-Page.py

--- practical-statistics-for-data-scientists/ANOVA-Multiple-Page.py
+++ practical-statistics-for-data-scientists/ANOVA-Multiple-Page.py
@@ -19,3 +19,14 @@ def plot_multiple_sessions(data):
     plt.title('')
     plt.tight_layout()
     plt.show()
+def permutation_variance_test(df, n_permutations=3000):
+    """Esegue test di permutazione sulla varianza"""
+    # Calcola varianza osservata
+    observed_variance = df.groupby('Page').mean().var()[0]
+    
+    # Funzione per singola permutazione
+    def perm_test(df):
+        df = df.copy()
+        df['Time'] = np.random.permutation(df['Time'].values)
+        return df.groupby('Page').mean().var()[0]
+    
