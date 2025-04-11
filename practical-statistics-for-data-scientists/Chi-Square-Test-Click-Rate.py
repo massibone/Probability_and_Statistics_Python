703972fb453920747442224adc2015fae1ac41be
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2025-04-11 08:15:58 +0200

    Update Chi-Square-Test-Click-Rate.py

--- practical-statistics-for-data-scientists/Chi-Square-Test-Click-Rate.py
+++ practical-statistics-for-data-scientists/Chi-Square-Test-Click-Rate.py
@@ -9,3 +9,10 @@ def load_click_data(file_path):
     click_rate = pd.read_csv(file_path)
     return click_rate.pivot(index='Click', columns='Headline', values='Rate')
 
+def chi_square(observed, expected):
+    """Calcola la statistica chi-quadrato"""
+    pearson_residuals = []
+    for row, expect in zip(observed, expected):
+        pearson_residuals.append([(observe - expect) ** 2 / expect
+                                 for observe in row])
+    return np.sum(pearson_residuals)
