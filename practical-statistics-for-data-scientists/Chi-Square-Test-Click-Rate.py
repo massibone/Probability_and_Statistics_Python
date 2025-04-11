55000b499b519efed9f180d4540965d83801659d
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2025-04-11 08:15:45 +0200

    Update Chi-Square-Test-Click-Rate.py

--- practical-statistics-for-data-scientists/Chi-Square-Test-Click-Rate.py
+++ practical-statistics-for-data-scientists/Chi-Square-Test-Click-Rate.py
@@ -1,1 +1,11 @@
+import pandas as pd
+import numpy as np
+from scipy import stats
+import random
+import matplotlib.pyplot as plt
+
+def load_click_data(file_path):
+    """Carica e prepara i dati dei click"""
+    click_rate = pd.read_csv(file_path)
+    return click_rate.pivot(index='Click', columns='Headline', values='Rate')
 
