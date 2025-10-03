20febbd4a951785f63c316ed07dcbd9dd4d032bc
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2025-10-03 09:33:19 +0200

    Update MultipleLinearRegression.py

--- practical-statistics-for-data-scientists/MultipleLinearRegression.py
+++ practical-statistics-for-data-scientists/MultipleLinearRegression.py
@@ -1,3 +1,12 @@
 '''
 Uses the California housing dataset to demonstrate regression with multiple predictors.
 '''
+
+# Multiple Linear Regression Example
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.datasets import fetch_california_housing
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import r2_score, mean_squared_error
