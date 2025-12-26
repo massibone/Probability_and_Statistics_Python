7302cba0174d32e865dfba74ef88487ce237f6bd
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2025-12-26 09:01:20 +0100

    Update 10perc&95perc.py

--- DescriptiveStatistics/10perc&95perc.py
+++ DescriptiveStatistics/10perc&95perc.py
@@ -1,8 +1,6 @@
 '''
 il percentile è un concetto statistico che viene utilizzato per descrivere la posizione relativa di un valore all'interno di una distribuzione di dati. In sostanza, il decimo percentile rappresenta il valore al di sotto del quale si trova il 10% dei dati, mentre il 95-esimo percentile rappresenta il valore al di sotto del quale si trova il 95% dei dati.
-
 Ad esempio, supponiamo di avere i seguenti dati relativi ai punteggi di un esame:
-
 punteggi = [60, 65, 70, 75, 80, 85, 90, 95, 100]
 
 Il decimo percentile rappresenterà il punteggio al di sotto del quale si trova il 10% dei dati. Nel nostro esempio, il 10% dei dati corrisponde a 0.1 * 9 = 0.9 (arrotondato a 1). Pertanto, il decimo percentile sarà il punteggio al di sotto del quale si trova il 10% dei dati, ovvero il punteggio corrispondente all'indice 1 cioe' 65
