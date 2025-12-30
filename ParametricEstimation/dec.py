d4fe12860e650c14e82cd203361d39599f547b68
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2025-12-30 09:50:40 +0100

    Update dec.py

--- ParametricEstimation/dec.py
+++ ParametricEstimation/dec.py
@@ -3,7 +3,6 @@ import matplotlib.pyplot as plt
 
 # Parametro della distribuzione esponenziale (tasso di decadimento)
 tasso_decadimento = 0.5  # Un valore a caso per l'esempio
-
 # Genera 1000 campioni dalla distribuzione esponenziale
 num_campioni = 1000
 tempi_attesa = np.random.exponential(scale=1/tasso_decadimento, size=num_campioni)
@@ -20,4 +19,5 @@ plt.xlabel('Tempo di attesa')
 plt.ylabel('Densità di probabilità')
 plt.title('Distribuzione Esponenziale (Tempi di Attesa)')
 plt.legend()
+
 plt.show()
